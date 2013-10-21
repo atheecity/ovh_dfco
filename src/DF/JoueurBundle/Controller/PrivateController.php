9ad31b62a04b55d1883514e22ece3cb5fcc07dea
Charles Molin <cmolin21@gmail.com>
2013-10-21 13:10:17 +0200

    Maj joueur import

--- src/DF/JoueurBundle/Controller/PrivateController.php
+++ src/DF/JoueurBundle/Controller/PrivateController.php
@@ -8,6 +8,7 @@ use Symfony\Component\HttpFoundation\Request;
 use JMS\SecurityExtraBundle\Annotation\Secure;
 use Symfony\Component\DomCrawler\Crawler;
 use DF\JoueurBundle\Entity\JoueurClub;
+use DF\JoueurBundle\Entity\Joueur;
 
 class PrivateController extends Controller
 {
@@ -29,37 +30,36 @@ class PrivateController extends Controller
 		$crawler = new Crawler(file_get_contents('http://www.lfp.fr/club/'. $club->getLfpLibelle()));
 		$crawler = $crawler->filter("#sfListeEffectif")->filter('.joueur');
 		
-		$joueursUrl = array();
 		foreach ($crawler as $cr) {
-			$joueur = new \DF\JoueurBundle\Entity\Joueur();
 			$cr = new Crawler($cr);
+			
 			$urlJoueurLfp = $cr->filter('.infojoueur a')->attr('href');
+			
+			//Test si le joueur existe déjà
 			$joueurProv = $this->getDoctrine()->getRepository('DFJoueurBundle:Joueur')->findOneByLfpJoueurUrl($urlJoueurLfp);
 			if (!is_null($joueurProv))
 				continue;
+			
+			$joueur = new Joueur();
 			$joueur->setLfpJoueurUrl($urlJoueurLfp);
 			$ficheJoueur = new Crawler(file_get_contents('http://www.lfp.fr'.$urlJoueurLfp, 'UTF-8'));
 			
-			$nomPrenom = explode(' ', utf8_decode($ficheJoueur->filter('#fiche')->filter('h1')->text()));
-			foreach ($nomPrenom as $text) {
-				if(ctype_upper($text))
-					$joueur->setNom(ucwords(strtolower($text)));
-				else
-					$joueur->setPrenom(ucwords(strtolower($text)));
-			}
+			//Récupère le nom et le prénom du joueur
+			$nomPrenom = explode(' ', $ficheJoueur->filter('#fiche')->filter('h1')->text());
+			$joueur->setNom(ucwords(strtolower($nomPrenom[1])));
+			$joueur->setPrenom(ucwords($nomPrenom[0]));
 			
 			$infoJoueur = $ficheJoueur->filter('.infos_joueur');
 			$i = 0;
 			foreach ($infoJoueur->filter('dt') as $dt) {
 				$dt = new Crawler($dt);
-				switch (utf8_decode($dt->html())) {
+				switch ($dt->html()) {
 					case 'Nationalité :':
-						$dd = utf8_decode($infoJoueur->filter('dd')->eq($i)->text());
+						$dd = $infoJoueur->filter('dd')->eq($i)->text();
 						$joueur->setNationalite($dd);
 						break;
 					case 'Né le :':
 						$dd = explode(' ', utf8_decode($infoJoueur->filter('dd')->eq($i)->text()));
-						var_dump( $dd);
 						$date = new \DateTime();
 						$date->setDate((int)$dd[3], (int)$this->_month($dd[2]), (int)$dd[1]);
 						var_dump($date);
@@ -117,21 +117,32 @@ class PrivateController extends Controller
 		if ($form->isValid()) {
 			$data = $form->getData();
 			$this->newEffectifAction($data['club']->getId());
+			
+			return $this->redirect($this->generateUrl('DFJoueurBundle_showJoueur'));
 		}
 		else
 			return $this->render('DFAdminBundle:Private:form.html.twig', array(
 				'form' => $form->createView(),
+				'titleCategorie' => 'Joueurs',
 				'title' => 'Choix club',
 			));
 	}
 	
+	/**
+	 * Liste des joueurs
+	 * @return \Symfony\Component\HttpFoundation\Response
+	 * 
+	 * @Secure(roles="ROLE_REDACTEUR")
+	 */
 	public function showJoueurAction()
 	{
 		$em = $this->getDoctrine()->getManager();
 		$joueurs = $this->getDoctrine()->getRepository('DFJoueurBundle:Joueur')->findAll();
 		
 		return $this->render('DFJoueurBundle:Private:show-joueur.html.twig', array(
-			'joueurs' => $joueurs
+			'joueurs' => $joueurs, 
+			'titleCategorie' => 'Joueurs', 
+			'title' => 'Liste des joueurs',
 		));
 	}
 	
